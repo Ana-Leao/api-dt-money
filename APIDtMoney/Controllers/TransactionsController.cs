62bd6635416c9aa7f866c222612ffa7547afa0bc
Ana Paula Queiroz Leao <5935963@seap.local>
2023-09-05 09:40:41 -0300

    tratamento de erros

--- APIDtMoney/Controllers/TransactionsController.cs
+++ APIDtMoney/Controllers/TransactionsController.cs
@@ -21,73 +21,108 @@ namespace APIDtMoney.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Transaction>> Get()
         {
-            //AsNoTracking otimiza consulta GET - somente leitura 
-            //Take() limita a consulta para o número especificado
-            var transactions = _context.Transactions.AsNoTracking().Take(20).ToList();
-
-            if (transactions is null)
+            try
             {
-                return NotFound("Contas não encontradas...");
+                //AsNoTracking otimiza consulta GET - somente leitura 
+                //Take() limita a consulta para o número especificado
+                var transactions = _context.Transactions.AsNoTracking().Take(20).ToList();
+
+                if (transactions is null)
+                {
+                    return NotFound("Contas não encontradas...");
+                }
+                return transactions;
             }
-            return transactions;
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao tratar a sua solução");
+            }
         }
 
         [HttpGet("{id:int}", Name = "GetBill")]
         public ActionResult<Transaction> Get(int id)
         {
-            var bill = _context.Transactions.FirstOrDefault(p => p.Id == id);
-
-            if (bill is null)
+            try
             {
-                return NotFound("Conta não encontrada...");
-            }
+                var bill = _context.Transactions.FirstOrDefault(p => p.Id == id);
 
-            return bill;
+                if (bill is null)
+                {
+                    return NotFound($"Transação '{id}' não encontrada...");
+                }
+
+                return bill;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao tratar a sua solução");
+            }
         }
 
         [HttpPost]
         public ActionResult Post(Transaction bill)
         {
-            if (bill is null)
+            try
             {
-                return BadRequest();
+                if (bill is null)
+                {
+                    return BadRequest("Dados inválidos.");
+                }
+
+                _context.Transactions.Add(bill);
+                _context.SaveChanges();
+
+                return new CreatedAtRouteResult("GetBill",
+                    new { id = bill.Id }, bill);
             }
-
-            _context.Transactions.Add(bill);
-            _context.SaveChanges();
-
-            return new CreatedAtRouteResult("GetBill",
-                new { id = bill.Id }, bill);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao tratar a sua solução");
+            }
         }
 
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, Transaction bill)
         {
-            if (id != bill.Id)
+            try
             {
-                return BadRequest("ID não corresponde!");
+                if (id != bill.Id)
+                {
+                    return BadRequest("ID não corresponde!");
+                }
+
+                _context.Entry(bill).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return Ok(bill);
             }
-
-            _context.Entry(bill).State = EntityState.Modified;
-            _context.SaveChanges();
-
-            return Ok(bill);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao tratar a sua solução");
+            }
         }
 
         [HttpDelete]
         public ActionResult Delete(int id)
         {
-            var bill = _context.Transactions.FirstOrDefault(p => p.Id == id);
-
-            if (bill is null)
+            try
             {
-                return NotFound("Conta não encontrada");
+                var bill = _context.Transactions.FirstOrDefault(p => p.Id == id);
+
+                if (bill is null)
+                {
+                    return NotFound("Conta não encontrada");
+                }
+
+                _context.Transactions.Remove(bill);
+                _context.SaveChanges();
+
+                return Ok(bill);
             }
-
-            _context.Transactions.Remove(bill);
-            _context.SaveChanges();
-
-            return Ok(bill);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um problema ao tratar a sua solução");
+            }
         }
 
     }
